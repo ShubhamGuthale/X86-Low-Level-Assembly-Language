9848d9879418cfffa67c89af1576272ca9d39d18
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-03-01 15:05:25 +0530

    Create Add Of Two Num Scanf.asm

---
+++ Practice/Add Of Two Num Scanf.asm
@@ -0,0 +1,39 @@
+	;; addition
+section .data
+        msg1 db "Enter first no.:",10,0
+	msg2 dd "%d",10
+	msg3 db "Enter second no.:",10,0
+	msg4 dd "%d",10
+	msg db "Your Addition Is : %d",10,0
+section .bss
+        a resd 1
+	b resd 1
+	n resd 1
+section .text
+        global main
+	extern printf,scanf
+
+main:
+	push msg1
+	call printf
+	add esp,4
+	push a
+	push msg2
+	call scanf
+	add esp,8
+	push msg3
+	call printf
+	add esp,4
+	push b
+	push msg4
+	call scanf
+	add esp,8
+	mov eax,dword[a]
+	mov ebx,dword[b]
+	add eax,ebx
+	;; mov dword[n],eax
+	push eax
+	push msg
+	call printf
+	add esp,8
+	ret
